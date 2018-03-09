59adc1e13f57e0e4e72b83409a1302cccdcb4d3f
Jeff Mitchell <jeffm@3doto.com>
2018-03-09 14:04:33 -0500

    Format code with Prettier.

--- wp-auth.js
+++ wp-auth.js
@@ -1,118 +1,160 @@
-var crypto = require( 'crypto' );
+var crypto = require('crypto');
 
-function WP_Auth( wpurl, logged_in_key, logged_in_salt,
-				mysql_host, mysql_user, mysql_pass, mysql_db,
-				wp_table_prefix ) {
-	var md5 = crypto.createHash( 'md5' );
-	md5.update( wpurl );
-	this.cookiename = 'wordpress_logged_in_' + md5.digest( 'hex' );
-	this.salt = logged_in_key + logged_in_salt;
+function WP_Auth(
+  wpurl,
+  logged_in_key,
+  logged_in_salt,
+  mysql_host,
+  mysql_user,
+  mysql_pass,
+  mysql_db,
+  wp_table_prefix
+) {
+  var md5 = crypto.createHash('md5');
+  md5.update(wpurl);
+  this.cookiename = 'wordpress_logged_in_' + md5.digest('hex');
+  this.salt = logged_in_key + logged_in_salt;
 
-	var mysql = require( 'mysql2' );
-	var mysqlOpts = {
-		connectionLimit : 10,
-		host: mysql_host,
-		database: mysql_db,
-		user: mysql_user,
-		password: mysql_pass
-	};
+  var mysql = require('mysql2');
+  var mysqlOpts = {
+    connectionLimit: 10,
+    host: mysql_host,
+    database: mysql_db,
+    user: mysql_user,
+    password: mysql_pass
+  };
 
-	this.db = mysql.createPool( mysqlOpts );
+  this.db = mysql.createPool(mysqlOpts);
 
-	this.db.on(' error', function( err ) {
-	  console.log( err.code ); // 'ER_BAD_DB_ERROR'
-	} );
+  this.db.on(' error', function(err) {
+    console.log(err.code); // 'ER_BAD_DB_ERROR'
+  });
 
-	this.table_prefix = wp_table_prefix;
+  this.table_prefix = wp_table_prefix;
 
-	this.known_hashes = {};
-	this.known_hashes_timeout = {};
-	this.meta_cache = {};
-	this.meta_cache_timeout = {};
+  this.known_hashes = {};
+  this.known_hashes_timeout = {};
+  this.meta_cache = {};
+  this.meta_cache_timeout = {};
 
-	// Default cache time: 5 minutes
-	this.timeout = 300000;
+  // Default cache time: 5 minutes
+  this.timeout = 300000;
 }
 
-WP_Auth.prototype.checkAuth = function( req ) {
-	var self = this, data = null;
-	if ( req.headers.cookie )
-		req.headers.cookie.split( ';' ).forEach( function( cookie ) {
-			if ( cookie.split( '=' )[0].trim() == self.cookiename )
-				data = cookie.split( '=' )[1].trim().split( '%7C' );
-		} );
-	else
-		return new Invalid_Auth("no cookie");
+WP_Auth.prototype.checkAuth = function(req) {
+  var self = this,
+    data = null;
+  if (req.headers.cookie)
+    req.headers.cookie.split(';').forEach(function(cookie) {
+      if (cookie.split('=')[0].trim() == self.cookiename)
+        data = cookie
+          .split('=')[1]
+          .trim()
+          .split('%7C');
+    });
+  else return new Invalid_Auth('no cookie');
 
-	if ( !data )
-		return new Invalid_Auth("no data in cookie");
+  if (!data) return new Invalid_Auth('no data in cookie');
 
-	if ( parseInt( data[1] ) < new Date / 1000 )
-		return new Invalid_Auth("expired cookie");
+  if (parseInt(data[1]) < new Date() / 1000)
+    return new Invalid_Auth('expired cookie');
 
-	return new Valid_Auth( data, this );
+  return new Valid_Auth(data, this);
 };
 
-exports.create = function( wpurl, logged_in_key, logged_in_salt,
-				mysql_host, mysql_user, mysql_pass, mysql_db,
-				wp_table_prefix ) {
-	return new WP_Auth( wpurl, logged_in_key, logged_in_salt,
-				mysql_host, mysql_user, mysql_pass, mysql_db,
-				wp_table_prefix );
+exports.create = function(
+  wpurl,
+  logged_in_key,
+  logged_in_salt,
+  mysql_host,
+  mysql_user,
+  mysql_pass,
+  mysql_db,
+  wp_table_prefix
+) {
+  return new WP_Auth(
+    wpurl,
+    logged_in_key,
+    logged_in_salt,
+    mysql_host,
+    mysql_user,
+    mysql_pass,
+    mysql_db,
+    wp_table_prefix
+  );
 };
 
-function Invalid_Auth(err) { this.err = err; }
-Invalid_Auth.prototype.on = function( key, callback ) {
-	if ( key != 'auth' )
-		return this;
-	var self = this;
-	process.nextTick( function() {
-		callback.call( self, false, 0, self.err );
-	} );
-	return this;
+function Invalid_Auth(err) {
+  this.err = err;
+}
+Invalid_Auth.prototype.on = function(key, callback) {
+  if (key != 'auth') return this;
+  var self = this;
+  process.nextTick(function() {
+    callback.call(self, false, 0, self.err);
+  });
+  return this;
 };
 
-function Valid_Auth( data, auth ) {
-	var self = this, user_login = data[0], expiration = data[1], token = data[2], hash = data[3];
-	user_login = user_login.replace('%40', '@');
+function Valid_Auth(data, auth) {
+  var self = this,
+    user_login = data[0],
+    expiration = data[1],
+    token = data[2],
+    hash = data[3];
+  user_login = user_login.replace('%40', '@');
 
-	if ( user_login in auth.known_hashes_timeout && auth.known_hashes_timeout[user_login] < +new Date ) {
-		delete auth.known_hashes[user_login];
-		delete auth.known_hashes_timeout[user_login];
-	}
+  if (
+    user_login in auth.known_hashes_timeout &&
+    auth.known_hashes_timeout[user_login] < +new Date()
+  ) {
+    delete auth.known_hashes[user_login];
+    delete auth.known_hashes_timeout[user_login];
+  }
 
-	function parse( pass_frag, id ) {
-		var hmac1 = crypto.createHmac( 'md5', auth.salt );
-        var key = user_login + '|' + pass_frag + '|' + expiration + '|' + token;
-        hmac1.update(key);
-        var hkey = hmac1.digest('hex');
-        var hmac2 = crypto.createHmac('sha256', hkey);
-        hmac2.update(user_login + '|' + expiration + '|' + token);
-        var cookieHash = hmac2.digest('hex');
-		if ( hash == cookieHash ) {
-			self.emit( 'auth', true, id );
-		} else {
-			self.emit( 'auth', false, 0, "invalid hash" );
-		}
-	}
-
-	if ( user_login in auth.known_hashes ) {
-		return process.nextTick(function() {
-			parse( auth.known_hashes[user_login].frag, auth.known_hashes[user_login].id );
-		} );
+  function parse(pass_frag, id) {
+    var hmac1 = crypto.createHmac('md5', auth.salt);
+    var key = user_login + '|' + pass_frag + '|' + expiration + '|' + token;
+    hmac1.update(key);
+    var hkey = hmac1.digest('hex');
+    var hmac2 = crypto.createHmac('sha256', hkey);
+    hmac2.update(user_login + '|' + expiration + '|' + token);
+    var cookieHash = hmac2.digest('hex');
+    if (hash == cookieHash) {
+      self.emit('auth', true, id);
+    } else {
+      self.emit('auth', false, 0, 'invalid hash');
     }
+  }
 
-	var query = 'select ID, user_pass from ' + auth.table_prefix + 'users where user_login = \'' + user_login.replace( /(\'|\\)/g, '\\$1' ) + '\'';
-	auth.db.query(query, function (err, data) {
-		if (err) {
-			auth.known_hashes[user_login] = {frag: '__fail__', id: 0};
-			auth.known_hashes_timeout[user_login] = +new Date + auth.timeout;
-		} else {
-			auth.known_hashes[user_login] = {frag: data[0].user_pass.substr( 8, 4 ), id: data[0].ID};
-			auth.known_hashes_timeout[user_login] = +new Date + auth.timeout;
-		}
-		parse( auth.known_hashes[user_login].frag, auth.known_hashes[user_login].id );
-	});
+  if (user_login in auth.known_hashes) {
+    return process.nextTick(function() {
+      parse(
+        auth.known_hashes[user_login].frag,
+        auth.known_hashes[user_login].id
+      );
+    });
+  }
+
+  var query =
+    'select ID, user_pass from ' +
+    auth.table_prefix +
+    "users where user_login = '" +
+    user_login.replace(/(\'|\\)/g, '\\$1') +
+    "'";
+  auth.db.query(query, function(err, data) {
+    if (err) {
+      auth.known_hashes[user_login] = { frag: '__fail__', id: 0 };
+      auth.known_hashes_timeout[user_login] = +new Date() + auth.timeout;
+    } else {
+      auth.known_hashes[user_login] = {
+        frag: data[0].user_pass.substr(8, 4),
+        id: data[0].ID
+      };
+      auth.known_hashes_timeout[user_login] = +new Date() + auth.timeout;
+    }
+    parse(auth.known_hashes[user_login].frag, auth.known_hashes[user_login].id);
+  });
 }
 
-require( 'util' ).inherits( Valid_Auth, require( 'events' ).EventEmitter );
+require('util').inherits(Valid_Auth, require('events').EventEmitter);
