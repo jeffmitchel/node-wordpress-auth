e0a49b5db939b8f503099e69357c5a79bdeae680
Jeff Mitchell <jeffm@3doto.com>
2018-03-09 14:01:59 -0500

    Remove functions to get and set user metadata.

--- README.md
+++ README.md
@@ -21,22 +21,3 @@ When you get a HTTP request and you need to verify auth:
         auth_is_valid; // true if the user is logged in, false if they are not
         user_id; // the ID number of the user or 0 if the user is not logged in
     } );
-
-To get a usermeta value:
-
-    wp_auth.getUserMeta( user_id, 'meta_key (eg. occupation)', function( data ) {
-        // data is null if the user doesn't exist or doesn't have the key.
-        // Otherwise, it is the value you would get in WordPress (unserialized intelligently)
-    } );
-
-To set a usermeta value:
-
-    wp_auth.setUserMeta( user_id, 'meta_key (eg. occupation)', 'meta_value (eg. Eating spaghetti)' );
-
-To get a user_id from a usermeta value:
-
-    wp_auth.reverseUserMeta( 'meta_key (eg. occupation)', 'meta_value (eg. Eating spaghetti)', function( id ) {
-        // id is null if no user could be found with the given meta key value pair
-        // Otherwise, it is the ID of the user
-        // If multiple users matched, only one will be given
-    } );
--- wp-auth.js
+++ wp-auth.js
@@ -1,24 +1,4 @@
-var crypto = require( 'crypto' ),
-	phpjs = require( './serialize' );
-
-function sanitizeValue( value ) {
-	switch ( typeof value ) {
-		case 'boolean':
-		case 'object':
-			return phpjs.serialize( value ).replace( /(\'|\\)/g, '\\$1' );
-		case 'number':
-			return Number.toString.call( value );
-		case 'string':
-			try {
-				// If it's a serialized string, serialize it again so it comes back out of the database the same way.
-				return phpjs.serialize( phpjs.serialize( phpjs.unserialize( value ) ) ).replace( /(\'|\\)/g, '\\$1' );
-			} catch ( ex ) {
-				return value.replace( /(\'|\\)/g, '\\$1' );
-			}
-		default:
-			throw new Error( 'Invalid data type: ' + typeof value );
-	}
-}
+var crypto = require( 'crypto' );
 
 function WP_Auth( wpurl, logged_in_key, logged_in_salt,
 				mysql_host, mysql_user, mysql_pass, mysql_db,
@@ -71,88 +51,6 @@ WP_Auth.prototype.checkAuth = function( req ) {
 		return new Invalid_Auth("expired cookie");
 
 	return new Valid_Auth( data, this );
-};
-
-WP_Auth.prototype.getUserMeta = function( id, key, callback ) {
-	if ( !( id in this.meta_cache_timeout ) )
-		this.meta_cache_timeout[id] = {};
-
-	if ( key in this.meta_cache_timeout[id] && this.meta_cache_timeout[id][key] < +new Date ) {
-		delete this.meta_cache[id][key];
-		delete this.meta_cache_timeout[id][key];
-	}
-
-	if ( id in this.meta_cache && key in this.meta_cache[id] ) {
-		callback( this.meta_cache[id][key] );
-		return;
-	}
-
-	var self = this;
-	this.db.query( 'select meta_value from ' + this.table_prefix + 'usermeta where meta_key = \'' + sanitizeValue( key ) + '\' and user_id = ' + parseInt( id ) ).on( 'row', function( data ) {
-		if ( !( id in self.meta_cache ) )
-			self.meta_cache[id] = {};
-		try {
-			self.meta_cache[id][key] = phpjs.unserialize( data.meta_value );
-		} catch( ex ) {
-			self.meta_cache[id][key] = data.meta_value;
-		}
-	} ).on( 'end', function() {
-		if ( !( id in self.meta_cache ) )
-			self.meta_cache[id] = {};
-		if ( !( key in self.meta_cache[id] ) )
-			self.meta_cache[id][key] = null;
-		self.meta_cache_timeout[id][key] = +new Date + self.timeout;
-		callback( self.meta_cache[id][key] );
-	} );
-};
-
-WP_Auth.prototype.setUserMeta = function( id, key, value ) {
-	if ( !( id in this.meta_cache_timeout ) )
-		this.meta_cache_timeout[id] = {};
-
-	this.meta_cache[id][key] = value;
-	this.meta_cache_timeout[id][key] = +new Date + this.timeout;
-
-	var sanitized_value = sanitizeValue( value );
-
-	var self = this;
-	this.db.query( 'delete from' + this.table_prefix + 'usermeta where meta_key = \'' + sanitizeValue( key ) + '\' and user_id = ' + parseInt( id ) );
-	this.db.query( 'insert into' + this.table_prefix + 'usermeta (meta_key, user_id, meta_value) VALUES(\'' + sanitizeValue( key ) + '\', ' + parseInt( id ) + ', \'' + sanitized_value + '\')' );
-};
-
-WP_Auth.prototype.reverseUserMeta = function( key, value, callback ) {
-	for ( var id in this.meta_cache ) {
-		if ( key in this.meta_cache[id] && this.meta_cache[id][key] == value ) {
-			callback( id );
-			return;
-		}
-	}
-
-	var id = null;
-
-	var self = this;
-	this.db.query( 'select user_id from ' + this.table_prefix + 'usermeta where meta_key = \'' + sanitizeValue( key ) + '\' and meta_value = \'' + sanitizeValue( key ) + '\'' ).on( 'row', function( data ) {
-		id = data.user_id;
-		if ( !( id in self.meta_cache ) )
-			self.meta_cache[id] = {};
-		if ( !( id in self.meta_cache_timeout ) )
-			self.meta_cache_timeout[id] = {};
-		self.meta_cache[id][key] = value;
-		self.meta_cache_timeout[id][key] = +new Date + this.timeout;
-	} ).on( 'end', function() {
-		callback( id );
-	} );
-};
-
-WP_Auth.prototype.getContributors = function( callback ) {
-	var self = this;
-    var users = [];
-	// this.db.query( 'select ID as user_id, user_login from ' + this.table_prefix + 'user' ).on( 'row', function( data ) {});
-	this.db.query( 'select user_id, user_login from ' + this.table_prefix + 'usermeta as meta left join ' + this.table_prefix + 'users as user on user.ID = meta.user_id where meta_key = \'wp_user_level\' and meta_value = \'1\'' ).on( 'row', function( data ) {
-        users.push( data );
-    }).on( 'end', function() {
-        callback( users );
-    });
 };
 
 exports.create = function( wpurl, logged_in_key, logged_in_salt,
